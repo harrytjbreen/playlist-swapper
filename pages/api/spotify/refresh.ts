edd21c5f56fd53bdfce8f7d9b12ad45b2f4123b7
Harry <harrytjbreen@gmail.com>
2022-05-12 13:44:00 +0100

    Fix: Fixed Refresh route

--- pages/api/spotify/refresh.ts
+++ pages/api/spotify/refresh.ts
@@ -4,23 +4,26 @@ import axios from "axios";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const clientID = process.env.SPOTIFY_ID as string;
   const clientSecret = process.env.SPOTIFY_SECRET as string;
-  const refreshToken = req.query.refreshToken as string;
+  const refreshToken = req.query.refresh as string;
 
   if(!refreshToken) {
     res.status(400).json({err: "Refresh token not supplied"})
   }
-  const data = {
+  const data = new URLSearchParams({
     grant_type: "refresh_token",
     refresh_token: refreshToken
-  }
+  });
+
   try {
     const response = await axios.post("https://accounts.spotify.com/api/token", data, {
       headers: {
-        'Authorization': 'Basic ' + (new Buffer(clientID + ':' + clientSecret).toString('base64'))
+        'Authorization': 'Basic ' + (new Buffer(clientID + ':' + clientSecret).toString('base64')),
+        'Content-Type': 'application/x-www-form-urlencoded'
       }
     });
     res.status(200).json({access: response.data.access_token})
   } catch (e) {
+    console.log(e);
     res.status(400).json({err: e})
   }
 
--- pages/spotify-callback.tsx
+++ pages/spotify-callback.tsx
@@ -1,16 +1,23 @@
 import type { NextPage } from 'next'
 import {useEffect} from "react";
 import axios from "axios";
+import useSpotifyAPI from "../hooks/useSpotifyAPI";
 
 const SpotifyCallback: NextPage = () => {
 
+  const {setTokens} = useSpotifyAPI();
 
   useEffect(() => {
     if(typeof window === "undefined") return;
     const queryParams = new URLSearchParams(window.location.search);
     const code = queryParams.get("code");
 
-  },[])
+    (async () => {
+      const res = await axios.get(`/api/spotify/get-access?code=${code}`);
+      setTokens(res.data);
+    })();
+
+  },[]);
 
   return (
     <>
